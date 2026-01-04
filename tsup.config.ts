a710e832926b6820a1622401e73790c481cf5a40
Alex Rempel <alexey.rempel@googlemail.com>
2026-01-04 16:14:56 +0100

    change to lib

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,8 +1,15 @@
 import {defineConfig} from 'tsup';
 
 export default defineConfig({
-  entry: {fp: 'src/fp/index.ts', array: 'src/array/index.ts', str: 'src/str/index.ts', common: 'src/common/index.ts'},
+  entry: {
+    array: 'src/array/index.ts',
+    common: 'src/common/index.ts',
+    date: 'src/date/index.ts',
+    fp: 'src/fp/index.ts',
+    str: 'src/str/index.ts',
+  },
   clean: true,
   format: ['cjs', 'esm'],
   dts: true,
+  outDir: 'lib',
 });
